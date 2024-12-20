64f48dd478769d93a88368a861bd34b326d61bf9
Youssef malki <malkiyoussefdigital@gmail.com>
2024-12-20 20:49:17 +0100

    premier commit

---
+++ variable.js
@@ -0,0 +1,44 @@
+// Variables: 
+
+
+/**
+ *تمرين 1:
+
+برمج متغيرات تخزن:
+
+    اسمك.
+    عمرك.
+    بلدك.
+
+ */
+
+    const name = "Youssef";
+    const age = 42;
+    const country = "Morrocco";
+
+    /**
+     Exemple:
+            // طباعة الأرقام من 1 إلى 5
+        for (let i = 1; i <= 5; i++) {
+        console.log(i);
+        }
+
+        // طباعة الأرقام حتى يصبح العدد أقل من 5
+        let count = 10;
+        while (count > 5) {
+        console.log(count);
+        count--;
+        }
+
+     */
+
+  /**
+   *   تمرين 2:
+
+    برمج حلقة تطبع الأرقام الزوجية من 1 إلى 10.
+    برمج حلقة تطبع حروف كلمة (JavaScript) واحد بواحد.
+   */
+
+    for(let i = 0; i <= 10; i+2){
+        console.log(i);
+    }
